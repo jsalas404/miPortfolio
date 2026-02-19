3154ff4d430c2b3e51246cb646b7ee5aeecd6c6a
jsalas <chiquidaw@gmail.com>
2026-02-19 15:59:19 +0000

    Datos de experiencia junto a su componente.

---
+++ src/data/experience.js
@@ -0,0 +1,16 @@
+export const experience = [
+    {
+      company: "Xaicom Comunicaciones",
+      role: "Desarrollador Web - Prácticas Dual GS",
+      period: "Mayo 2025 - Junio 2025",
+      description: "Desarrollo y diseño de diversas páginas web, con un enfoque especializado en configuración de WordPress y optimización de posicionamiento SEO local.",
+      tags: ["WordPress", "SEO", "Desarrollo Web"],
+    },
+    {
+      company: "Grupo Piñero (Hoteles Bahía Príncipe)",
+      role: "Tecnico Informático - Prácticas GM",
+      period: "Abril 2024 - Junio 2024",
+      description: "Mantenimiento técnico de equipos locales y actualización de infraestructura IT. Soporte a usuarios, organización de esquemas de red y testeo de conectividad Wifi.",
+      tags: ["Soporte IT", "Redes", "Infraestructura"],
+    },
+]
