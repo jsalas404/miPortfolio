a9c3c4854e3940182ef7c719077a96ad78e44b33
jsalas <chiquidaw@gmail.com>
2026-02-19 17:31:01 +0000

    Despliegue en GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://jsalas404.github.io",
+  base: "/miPortfolio",
   vite: {
     plugins: [tailwindcss()]
   }
